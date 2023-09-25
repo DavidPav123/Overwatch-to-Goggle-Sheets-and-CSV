c0d09d5ddcdcb69b5fe8a44ff2a5b215e08a13ca
DavidPav123 <davidpavlicek123@gmail.com>
2023-09-25 13:42:28 -1000

    Initalizing project

---
+++ src/google_sheets_push.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+from os.path import exists
+from json import load
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.errors import HttpError
+from googleapiclient import discovery
+
+# Scopes the program is allowed to access
+SCOPES: list[str] = ["https://www.googleapis.com/auth/spreadsheets"]
+
+def get_spreadsheet_id():
+    with open("config.json", 'r') as file:
+        data = load(file)
+        return data.get("Spreadsheet ID", None)
+
+# ID of spreadsheet to modify
+SPREADSHEET_ID: str = get_spreadsheet_id()
+
+def update_sheet(values_to_update, range_to_update) -> None:
+    creds: Credentials = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if exists("files/token.json"):
+        creds = Credentials.from_authorized_user_file("files/token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow: InstalledAppFlow = InstalledAppFlow.from_client_secrets_file(
+                "files/credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("files/token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = discovery.build("sheets", "v4", credentials=creds)
+
+        value_range_body: dict = {"values": values_to_update}
+
+        request = (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=SPREADSHEET_ID,
+                range=range_to_update,
+                valueInputOption="USER_ENTERED",
+                body=value_range_body,
+            )
+        )
+        response = request.execute()
+    except HttpError as err:
+        print(err)
---
+++ src/stats_to_GS.py
@@ -0,0 +1,148 @@
+from glob import glob
+from os.path import getctime, expanduser
+from csv import reader
+from time import sleep
+from csv import writer
+from google_sheets_push import update_sheet
+import platform
+
+# List of pages for google sheets to write to
+pages_to_update: str = [
+    "Placeholder",  # DO NOT DELETE
+    "Placeholder2",  # DO NOT DELETE
+    "Sheet1",  # Change to page 1
+    "Sheet2",  # Change to page 2
+    "Sheet3",  # Change to page 3
+    "Sheet4",  # Change to page 4
+    "Sheet5",  # Chnage to page 5
+]
+# Variable for switching pages when new file is detected
+current_page: int = 0
+# Range to update in sheets change infor after
+range_name: str = f"{pages_to_update[current_page]}!A1:Z26"
+# Name of the current map
+cur_map: str = ""
+
+
+"""def get_latest_file() -> str:
+    #Use for normal douments folder
+    #list_of_files: list[str] = glob(f"{expanduser('~/Documents')}/Overwatch/Workshop/*")
+    list_of_files: list[str] = glob(f"{expanduser('~/')}/OneDrive/Documents/Overwatch/Workshop/*")
+    latest_file: str = max(list_of_files, key=getctime)
+    return latest_file""" 
+
+def get_latest_file() -> str:
+    win_version = platform.release()
+    list_of_files = []
+    
+    if win_version == "10":
+        list_of_files = glob(f"{expanduser('~/Documents')}/Overwatch/Workshop/*")
+    elif win_version == "11":
+        list_of_files = glob(f"{expanduser('~/')}/OneDrive/Documents/Overwatch/Workshop/*")
+    else:
+        print(f"This is neither Windows 10 nor Windows 11. Detected version: {win_version}")
+
+    latest_file: str = max(list_of_files, key=getctime)
+    return latest_file
+
+
+def read_csv_file(file_to_read: str) -> list:
+    row_data: list[list] = []
+    t1: list = []
+    t2: list = []
+    file_length: int = file_len(file_to_read)
+
+    with open(file_to_read) as csv_file:
+        csv_reader = reader(csv_file, delimiter=",")
+        for numbers in range(file_length - 9, file_length + 1):
+            for rows in csv_reader:
+                if csv_reader.line_num == numbers:
+                    rows.pop(0)
+                    if rows[1] == "LÃºcio":
+                        rows[1] = "Lucio"
+                    elif rows[1] == "TorbjÃ¶rn":
+                        rows[1] = "Torbjorn"
+                    if rows[len(rows) - 1] == "Team 1":
+                        t1.insert(0, rows)
+                    else:
+                        t2.append(rows)
+                    break
+
+    row_data.insert(
+        0,
+        [
+            "Player Name",
+            "Hero Name",
+            "Damage Dealt",
+            "Barrier Damage",
+            "Damage Blocked",
+            "Damage Taken",
+            "Deaths",
+            "Elims",
+            "Final Blows",
+            "Env Deaths",
+            "Env Kills",
+            "Healing",
+            "Obj Kills",
+            "solo kills",
+            "Ults Earned",
+            "Ults Used",
+            "Healing Recived",
+            "Team",
+        ],
+    )
+    for t1_rows in t1:
+        row_data.append(t1_rows)
+    for t2_rows in t2:
+        row_data.append(t2_rows)
+    return row_data
+
+
+def check_file_change(file_to_read: str) -> list[str]:
+    with open(file_to_read) as csv_file:
+        csv_reader = reader(csv_file, delimiter=",")
+        header: list[str] = next(csv_reader)
+        return header
+
+
+def file_len(file_to_read: str) -> int:
+    with open(file_to_read) as fp:
+        lines: int = len(fp.readlines())
+        return lines
+
+
+def update_page(page_list: list, current_page: int) -> str:
+    return f"{page_list[current_page]}!A1:Z26"
+
+def export_to_csv(rows,file_name):
+    with open(f'CSVs/{file_name}', 'w') as f:
+        write = writer(f, delimiter=',', lineterminator="\n")
+        write.writerows(rows)
+
+if __name__ == "__main__":
+    file: str = get_latest_file()
+
+    cur_map_temp: list[str] = check_file_change(file)
+
+    while True:
+        cur_map_temp: list[str] = check_file_change(file)
+
+        if cur_map != cur_map_temp:
+            cur_map = cur_map_temp
+            current_page += 1
+            range_name = update_page(pages_to_update, current_page)
+
+        if 12 <= file_len(file):
+            stats = read_csv_file(file)
+            export_to_csv(stats, f"{current_page}.csv")
+            update_sheet(stats, range_name)
+
+        else:
+            print("Waiting for data")
+
+        new_file: str = get_latest_file()
+
+        if file != new_file:
+            file = new_file
+
+        sleep(2)
